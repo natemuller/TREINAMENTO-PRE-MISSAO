3f0a2a8285fa4b64822ec8763c7ea25b2d03717b
natan gostosao <natan.muller@edu.pucrs.br>
2025-04-07 18:54:58 -0300

    parte 2 implementada no arquivo Pt2.java

--- Treinamento-Pre_Missao/src/Pt1.java
+++ Treinamento-Pre_Missao/src/Pt1.java
@@ -1,8 +1,7 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class App {
+public class Pt1 {
     public static void main(String[] args) throws Exception {
 
         Random random = new Random();
---
+++ Treinamento-Pre_Missao/src/Pt2.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class Pt2 {
+
+    public static void main(String[] args) {
+        
+        ArrayList<String> codinomes = new ArrayList<String>();
+
+        codinomes.add("Natan");
+        codinomes.add("Felipe");
+        codinomes.add("Luiz");
+        codinomes.add("Gabriel");
+        codinomes.add("Emile");
+        codinomes.add("Natan");
+        codinomes.add("Felipe");
+        codinomes.add("Luiz");
+        codinomes.add("Gabriel");
+        codinomes.add("Emile");
+        codinomes.add("Lisboa");
+        codinomes.add("Lisboa");
+        codinomes.add("Lisboa");
+        codinomes.add("Silvio Santos");
+
+        System.out.printf("Lista inteira com elementos repetidos:\n");
+
+        for (int i = 0; i < codinomes.size(); i++) {
+            System.out.println(" - " + codinomes.get(i));
+        }
+
+        for (int i = 0; i < codinomes.size(); i++) {
+            for (int j = i + 1; j < codinomes.size(); j++) {
+                if (codinomes.get(i).equals(codinomes.get(j))) {
+                    codinomes.remove(j);
+                    j--;
+                }
+            }
+        }
+
+        System.out.printf("Lista sem duplicatas:\n");
+
+        for (int i = 0; i < codinomes.size(); i++) {
+            System.out.println(" - " + codinomes.get(i));
+        }
+
+        if (codinomes.contains("Lisboa")) {
+            int i = codinomes.indexOf("Lisboa");
+            codinomes.set(i, "Valencia");
+        }
+
+        System.out.printf("Lista em ordem alfabetica:\n");
+
+        codinomes.sort(null);
+
+        for (int i = 0; i < codinomes.size(); i++) {
+            System.out.println(" - " + codinomes.get(i));
+        }
+    }
+}
